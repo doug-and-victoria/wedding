5798df6e13b66bddb8879007fc532579b46c2775
Victoria Albanese <AlbaneseVi14@gmail.com>
2022-09-02 21:12:39 -0400

    added dynamic number of days until the wedding to the website

---
+++ js/home.js
@@ -0,0 +1,9 @@
+window.onload = function() 
+{
+    let wedding_day = new Date('09/10/2023');
+    let today = new Date();
+
+    let difference = wedding_day.getTime() - today.getTime();
+    let days_until_wedding = Math.ceil(difference / (1000 * 3600 * 24));
+    document.getElementById("days").innerHTML = days_until_wedding;
+} 
