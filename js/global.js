e09deb561bfe3a3fc76b8bb49764986f1a7713a0
Victoria Albanese <AlbaneseVi14@gmail.com>
2023-05-09 22:16:46 -0400

    playing around with font size, orientation, & subtitle

--- js/global.js
+++ js/global.js
@@ -2,13 +2,31 @@ window.addEventListener("load", set_header);
 
 function set_header() 
 {
-    var header_div = document.createElement("div");
-    header_div.className = "title"
-    header_div.innerText = "Victoria & Douglas"
+    var title = document.createElement("p");
+    title.className = "title"
+    title.innerText = "Victoria Albanese & Douglas Salvati"
+
+    var subtitle = document.createElement("p");
+    subtitle.className = "subtitle"
+    subtitle.innerText = "September 10, 2023"
+
+    var titles_div = document.createElement("div");
+    titles_div.className = "titles_div"
+    titles_div.appendChild(title)    
+    titles_div.appendChild(subtitle)    
 
     var logo = document.createElement("img");
     logo.className = "wedding_logo"
     logo.src = "/wedding/images/logo_white.png"
+    
+    var logo_div = document.createElement("div");
+    logo_div.className = "wedding_logo_div"
+    logo_div.appendChild(logo)
+
+    var above_nav_stripe_div = document.createElement("div");
+    above_nav_stripe_div.className = "above_nav_stripe_div"
+    above_nav_stripe_div.appendChild(titles_div)
+    above_nav_stripe_div.appendChild(logo_div)
 
     var nav_stripe = document.createElement("nav");
     nav_stripe.innerHTML+= "\t\t<div class=\"nav_item\"><a href=\"/wedding/\"><span>Home</span></a></div>\n"
@@ -19,7 +37,6 @@ function set_header()
     nav_stripe.innerHTML+= "\t\t<div class=\"nav_item\"><a href=\"/wedding/pages/accommodations/\"><span>Accommodations</span></a></div>\n"
 
     var header = document.getElementsByTagName("header")[0]
-    header.appendChild(header_div)
-    header.appendChild(logo)
+    header.appendChild(above_nav_stripe_div)
     header.appendChild(nav_stripe)
 }
