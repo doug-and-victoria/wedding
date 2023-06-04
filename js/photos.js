4c8d4a2d54af9da86eea31cb335f7704863c3d22
doug-salvati <dougiesal@gmail.com>
2023-06-04 11:12:06 -0400

    fix: dont scroll when closing lightbox

--- js/photos.js
+++ js/photos.js
@@ -1,9 +1,12 @@
 window.onclick = e => 
 {
-    var src = e.target.src
-    var href = e.target.parentElement.href.split('#')[1];
-        
-    var lightbox = document.getElementsByClassName('lightbox')[0];
-    lightbox.id = href;
-    lightbox.childNodes[1].style = 'background-image: url(\'' + src + '\')'
+    // check if clicking an image
+    if (e.target.src) {
+        var src = e.target.src
+        var href = e.target.parentElement.href.split('#')[1];
+            
+        var lightbox = document.getElementsByClassName('lightbox')[0];
+        lightbox.id = href;
+        lightbox.childNodes[1].style = 'background-image: url(\'' + src + '\')'
+    }
 } 
